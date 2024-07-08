b52d7637a8ec6b051766293be09a2c7069663003
ShadowVirtual <61156296+ShadowVirtual@users.noreply.github.com>
2024-07-08 10:26:40 +0000

    Update

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ def main():
         sys.exit(1)
 
     script_file = sys.argv[1]
-    discord_token = sys.argv[2]
+    discord_bot_token = sys.argv[2]
 
     with open(script_file, 'r') as file:
         code = file.read()
@@ -18,7 +18,7 @@ def main():
     parser = Parser(tokenizer)
     statements = parser.parse()
     interpreter = Interpreter()
-    interpreter.variables['botToken'] = discord_token  # Set the bot token
+    interpreter.variables['botToken'] = discord_bot_token  # Set the bot token
     interpreter.interpret(statements)
 
 if __name__ == '__main__':
